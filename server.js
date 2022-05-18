5167fd991ed3ed37978ec2c8ee3f6f53e3e585ac
Natan Rajchenberg <natan.rajch@gmail.com>
2022-05-18 16:36:20 -0300

    ordenamiento archivos y simplificacion rutas

--- server.js
+++ server.js
@@ -12,22 +12,22 @@ app.set('view engine', 'handlebars');
 app.set('views', './views');
 
 app.get('/', (req, res) => {
-    res.render('home', {layout : 'main'});
+    res.render('subteflow', {layout : 'main'});
 });
 
-app.get('/am', (req, res) => {
+/* app.get('/am', (req, res) => {
     res.render('amounts', {layout : 'main'});
 });
 
 app.get('/sf', (req, res) => {
     res.render('subteflow', {layout : 'main'});
-});
+}); */
 
 /* app.get('/', (req, res) => res.send('Hello World !')) */
-app.get('/api/data', (req, res) => {
+/* app.get('/api/data', (req, res) => {
     const data = [100, 50, 300, 40, 350, 250, 20]; // assuming this is coming from the database
     res.json(data);
-});
+}); */
 
 
 //app.listen(3000);
